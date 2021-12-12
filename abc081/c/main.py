ee9fc5bcd934970e064c26ede513766f55afcd22
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2021-12-12 17:52:05 +0900

    solve 081

---
+++ abc081/c/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import collections
+import sys
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right  # type: ignore
+from collections import Counter, defaultdict, deque  # type: ignore
+from fractions import gcd  # type: ignore
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace, merge  # type: ignore
+from itertools import accumulate, combinations, permutations, product  # type: ignore
+from numba import njit
+
+
+def InputLI():
+    return list(map(int, sys.stdin.buffer.readline().split()))
+
+
+def InputI():
+    return int(sys.stdin.buffer.readline())
+
+
+def InputLS():
+    return sys.stdin.buffer.readline().rstrip().decode("utf-8").split()
+
+
+def InputS():
+    return sys.stdin.buffer.readline().rstrip().decode("utf-8")
+
+
+def InputIR(n):
+    return [InputI() for _ in range(n)]
+
+
+def InputLIR(n):
+    return [InputLI() for _ in range(n)]
+
+
+def InputSR(n):
+    return [InputS() for _ in range(n)]
+
+
+def InputLSR(n):
+    return [InputLS() for _ in range(n)]
+
+
+def InputSRL(n):
+    return [list(InputS()) for _ in range(n)]
+
+
+def InputMSRL(n):
+    return [[int(i) for i in list(InputS())] for _ in range(n)]
+
+
+N, K = InputLI()
+
+A = InputLI()
+
+c = collections.Counter(A)
+
+l = sorted(c.values(), reverse=True)
+
+print(0 if len(l) <= K else sum(l[K:]))
